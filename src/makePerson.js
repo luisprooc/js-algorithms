a7c5e23aa55834242954ec618b6f5a1e408cd2f8
Luis Rosario <luisprooc@gmail.com>
2021-07-07 08:55:34 -0400

    [ADD] make a person finished successfully

--- src/makePerson.js
+++ src/makePerson.js
@@ -1,11 +1,31 @@
-class Person {
-    constructor(firstAndLast) {
-        this.getFirstName = () => firstAndLast.slice(0,firstAndLast.indexOf(" "));
-        this.getLastName = () => firstAndLast.slice(firstAndLast.indexOf(" ") + 1);
-        this.getFullName = () => firstAndLast;
+function Person (firstAndLast){
+    let names = {
+        firstName : firstAndLast.slice(0,firstAndLast.indexOf(" ")), 
+        lastName : firstAndLast.slice(firstAndLast.indexOf(" ") + 1)
+        
     }
+
+    this.getFirstName = () => names.firstName;
+    this.getLastName = () => names.lastName;
+    this.getFullName = () => `${names.firstName} ${names.lastName}`;
+    
+    this.setFirstName = (firstName) => {
+        names.firstName = firstName;
+    }
+    this.setLastName = (lastName) => {
+        names.lastName = lastName;
+    }
+
+    this.setFullName = (firstAndLast) => {
+        names.firstName = firstAndLast.slice(0,firstAndLast.indexOf(" ")); 
+        names.lastName = firstAndLast.slice(firstAndLast.indexOf(" ") + 1);
+    }
+    
 }
+
   
 const bob = new Person('Bob Ross');
 console.log(bob.getFirstName());
-console.log(bob.getFullName());
+bob.setFirstName("Rass");
+console.log(bob.getFirstName());
+console.log(Object.keys(bob).length);
