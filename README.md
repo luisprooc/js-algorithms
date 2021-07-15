a364e1f845d24079bd302427114cafbf367ede76
Luis Rosario <luisprooc@gmail.com>
2021-07-15 13:56:39 -0400

    [ADD] spinal case finished

--- README.md
+++ README.md
@@ -746,3 +746,22 @@ fearNotLetter("bcdf") should return the string e.
 fearNotLetter("abcdefghijklmnopqrstuvwxyz") should return undefined.
 
 ```
+
+
+## Spinal Tap Case
+
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+
+```javascript
+spinalCase("This Is Spinal Tap") should return the string this-is-spinal-tap.
+
+spinalCase("thisIsSpinalTap") should return the string this-is-spinal-tap.
+
+spinalCase("The_Andy_Griffith_Show") should return the string the-andy-griffith-show.
+
+spinalCase("Teletubbies say Eh-oh") should return the string teletubbies-say-eh-oh.
+
+spinalCase("AllThe-small Things") should return the string all-the-small-things.
+
+```
--- src/spinalCase.js
+++ src/spinalCase.js
@@ -1,10 +1,21 @@
 function spinalCase(str) {
-    const regex = new RegExp(/_|-|\s/,"g"); 
-    if(regex.test(regex)){
-        return str.split(regex).join("-").toLowerCase();
+    let newStr = str;
+    newStr = newStr.replace(/[\s|_]/g,"-");
+    if(/[a-z]+[A-Z]/.test(newStr)){
+      let copyStr = newStr[0];
+      
+      for(let i = 1; i < newStr.length; ++i){
+        if(newStr[i].toUpperCase() === newStr[i] && newStr[i] !== "-"){
+          if(newStr[i -1] !== "-"){
+            copyStr += "-";
+          }
+        }
+        copyStr += newStr[i];
+        
+      }
+      return copyStr.toLowerCase();
     }
-}
+    return newStr.toLowerCase();
+  }
   
-console.log(spinalCase('thisIsSpinalTap'));
-console.log(spinalCase('this IsSpinal Tap'));
-console.log(spinalCase('Teletubbies say Eh-oh"'));
+  console.log(spinalCase('AllThe-small Things'));
