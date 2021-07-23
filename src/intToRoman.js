0dd0a085f75a39c41eed137ed063081d42b94f16
Luis Rosario <luisprooc@gmail.com>
2021-07-23 10:02:59 -0400

    [ADD] Int To roman added

--- src/intToRoman.js
+++ src/intToRoman.js
@@ -1,76 +1,100 @@
-import intGenerator from "../generators/intGenerator.js";
 /**
  * @param {number} num
  * @return {string}
  */
 
+const checkValue = (num) => {
+    const str = num.toString(10);
+    return str[0] === "4" || str[0] === "9";
+}
+
 
 const intToRoman = (num) => {
-    const dict = {
+    const symbols = {
         1: "I", 5: "V", 10: "X",50: "L", 
         100: "C", 500: "D", 1000: "M"
     };
 
     let roman = "";
 
-
     while(num){
 
-        if(num < 5){
-            if(num === 4){
-                roman += dict[1] + dict[5];
-                num -= 4;
+        if(num >= 1000){
+            roman += symbols[1000];
+            num -= 1000;
+        }
 
+        else if(num >= 500){
+            if(checkValue(num)){
+                roman += symbols[100] + symbols[1000];
+                num -= 900;
             }
-            
             else{
-                roman += dict[1];
-                num -= 1;
-
+                roman += symbols[500];
+                num -= 500;
             }
         }
 
-        else if(num < 10){
+        else if(num >= 100){
+            if(checkValue(num)){
+                roman += symbols[100] + symbols[500];
+                num -= 400;
+            }
+            else{
+                roman += symbols[100];
+                num -= 100;
+            }
+        }
 
-            if(num === 9){
-                roman += dict[1] + dict[10];
+        else if(num >= 50){
+            if(checkValue(num)){
+                roman += symbols[10] + symbols[100];
+                num -= 90;
+            }
+            else{
+                roman += symbols[50];
+                num -= 50;
+            }
+        }
+
+        else if(num >= 10){
+            if(checkValue(num)){
+                roman += symbols[10] + symbols[50];
+                num -= 40;
+            }
+            else{
+                roman += symbols[10];
+                num -= 10;
+            }
+        }
+
+        else if(num >= 5){
+            if(checkValue(num)){
+                roman += symbols[1] + symbols[10];
                 num -= 9;
             }
-
-            else {
-                roman += dict[5];
+            else{
+                roman += symbols[5];
                 num -= 5;
             }
         }
 
-        else if(num < 50){
-            roman += dict[10];
-            num -= 10;
-        }
-
-        else if(num < 100){
-            roman += dict[50];
-            num -= 50;
-        }
-
-        else if(num < 500){
-            roman += dict[100];
-            num -= 100;
-        }
-
-        else if(num < 1000){
-            roman += dict[500];
-            num -= 500;
-        }
-
         else{
-            roman += dict[1000];
-            num -= 1000;
+            if(checkValue(num)){
+                roman += symbols[1] + symbols[5];
+                num -= 4;
+            }
+            else{
+                roman += symbols[1];
+                num -= 1;
+            }
         }
+
     }
 
+    
     return roman;
 }
-//intGenerator(9)
 
-console.log( intToRoman(89) );
+
+console.log(intToRoman(83));
