4b5fae9ccb02fed760ea99c699f41b9a52e15a67
Luis Rosario <luisprooc@gmail.com>
2021-07-23 10:31:39 -0400

    [ADD] Steamroller added

--- README.md
+++ README.md
@@ -854,3 +854,22 @@ palindrome("0_0 (: /-\ :) 0-0") should return true.
 palindrome("five|\_/|four") should return false.
 
 ```
+
+
+## Steamroller
+
+Flatten a nested array. You must account for varying levels of nesting.
+
+
+```javascript
+steamrollArray([[["a"]], [["b"]]]) should return ["a", "b"].
+
+steamrollArray([1, [2], [3, [[4]]]]) should return [1, 2, 3, 4].
+
+steamrollArray([1, [], [3, [[4]]]]) should return [1, 3, 4].
+
+steamrollArray([1, {}, [3, [[4]]]]) should return [1, {}, 3, 4].
+
+Your solution should not use the Array.prototype.flat() or Array.prototype.flatMap() methods.
+
+```
--- src/steamroller.js
+++ src/steamroller.js
@@ -1,31 +1,11 @@
-function flatten(arr){
-  let newArr = []
-  if(!arr.length){
-    return newArr;
-  }
-
-  else if(typeof(arr) !== "object"){
-    newArr.push(arr);
-    return newArr + flatten(arr) + 1;
-  }
-
-  else{
-    return flatten(arr.splice(1))
-  }
-}
-
 function steamrollArray(arr) {
-    let newArr = [];
-    for(let i = 0; i < arr.length; ++i){
-      if(arr[i].length && typeof(arr[i]) !== "string"){
-        newArr.push(arr[i]);
-      }
-      else{
-        console.log(flatten(arr));
-      }
-    }
-    return newArr;
+  const newArr = arr.reduce((accumulator,iterator)=>
+    Array.isArray(iterator)
+    ? [...accumulator , ...steamrollArray(iterator)]
+    : [...accumulator,iterator]
+  ,[]);
+  return newArr;
 }
 
-console.log(flatten([3, [[4]]]));
-//console.log(steamrollArray([1,[2], [3, [[4]]]]));
+
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
